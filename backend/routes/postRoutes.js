7aea49e5a2f4b894277e20230199131675167083
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-05-26 23:23:49 +0530

    Implement REST API with user and post functionalities

---
+++ backend/routes/postRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+  createPost,
+  getPost,
+  deletePost,
+  likeUnlikePost,
+  replyToPost,
+  getFeedPosts,
+} from "../controllers/postController.js";
+import protectRoute from "../middlewares/protectRoute.js";
+
+const router = express.Router();
+
+router.get("/feed", protectRoute, getFeedPosts);
+router.get("/:id", protectRoute, getPost);
+router.post("/create", protectRoute, createPost);
+router.delete("/:id", protectRoute, deletePost);
+router.put("/like/:id", protectRoute, likeUnlikePost);
+router.put("/reply/:id", protectRoute, replyToPost);
+
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/userRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+
+dotenv.config();
+connectDB();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Routes
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+
+app.listen(PORT, () =>
+  console.log(`Server started at http://localhost:${PORT}`)
+);
